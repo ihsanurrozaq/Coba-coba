a1415e221682ed81220b1dfe8c66cae09232092f
Eksan Rozaq <ihsanurrozaq@gmail.com>
2021-04-26 12:29:27 +0700

    update

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ const data = {
       kelas: "IPA"
     },
     {
-      id: 4,
+      id: 5,
       nama: "eksan",
       kelas: "IPA"
+    },
+    {
+      id: 1,
+      nama: "jarwo",
+      kelas: "IPS"
     },
     {
       id: 3,
@@ -22,13 +27,54 @@ const data = {
   ]
 };
 
-console.table(data.mhs.map(i => ({nama: i.nama, kelas: i.kelas})));
-console.table(data.mhs.filter(x => x.kelas=="IPA"));
-console.table(data.mhs.map((a, index) => {
-  for (let i = 0; i < data.mhs.length; i++) {
-    if (a >=)
-  }
-}));
+console.table(data.mhs.map((i) => ({ nama: i.nama, kelas: i.kelas })));
+console.table(data.mhs.filter((x) => x.kelas == "IPA"));
+let loop1 = 0;
+let filter = (nama) => {
+  data.mhs.map((arr) => {
+    data.mhs.map((a, i) => {
+      if (nama[i + 1] !== undefined) {
+        if (nama[i].id > nama[i + 1].id) {
+          let temp = nama[i];
+          nama[i] = nama[i + 1];
+          nama[i + 1] = temp;
+        }
+      }
+      loop1++;
+    });
+  });
+  return nama;
+};
+console.table(filter(data.mhs), loop1);
+
+let mhsNew = data.mhs;
+const arrSorted = [];
+
+let loop = 0;
+
+const sorting = (arr) => {
+  if (arr.length == 0) return;
+  let smaller = 0;
+  let index = -1;
+  mhsNew.forEach((x, arrIndex) => {
+    if (arrIndex == 0) {
+      smaller = x.id;
+      index = arrIndex;
+    }
+
+    if (smaller > x.id) {
+      smaller = x.id;
+      index = arrIndex;
+    }
+    loop++;
+  });
+  arrSorted.push(mhsNew[index]);
+  mhsNew.splice(index, 1);
+  sorting(mhsNew);
+};
+
+sorting(mhsNew);
+console.table(arrSorted, loop);
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
